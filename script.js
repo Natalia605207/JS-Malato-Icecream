ac41f1314acfe4c8b7b0fb23d603a4a5f43b8e07
Natalia605207 <natalyaobukhova87@gmail.com>
2023-10-29 15:18:25 +0300

    Malato Ice cream website

---
+++ script.js
@@ -0,0 +1,25 @@
+let hamburger = document.querySelector(".hamburger");
+let navLinks = document.querySelector("#nav-links");
+let links = document.querySelectorAll(".links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("hide");
+    hamburger.classList.toggle("lines-rotate");
+})
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", () => {
+        navLinks.classList.toggle("hide");
+        hamburger.classList.toggle("lines-rotate");
+    })
+}
+
+gsap.from(".nav-container", {y: -50, ease: "power2.inOut", duration: 2})
+gsap.from(".top-icecream", {opacity: 0, duration: 3, delay: 1})
+gsap.from(".heading", {y: 50, ease: "back", duration: 1, opacity: 0, delay: 2})
+gsap.from("h2", {y: 50, ease: "back", duration: 1, opacity: 0, delay: 2.5})
+gsap.from(".top", {y: 50, ease: "back", duration: 1, opacity: 0, delay: 3})
+gsap.from(".top-icons", {opacity: 0, duration: 1.7, delay: 3.5})
+
+
+
